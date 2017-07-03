24e6367a54def834fd9362a7ea03b52c00c3c9eb
nithinsivadas <nithin@bu.edu>
2017-07-03 16:46:59 -0400

    Changed path of GeoDataMATLAB\setup.m

--- Tools/Misc/initialize_geodata.m
+++ Tools/Misc/initialize_geodata.m
@@ -2,6 +2,6 @@ function [] = initialize_geodata(filePath)
 %initialize_geodata.m The function initializes GeoData, given the path
 %where the GeoData setup.m is stored
 rootPathStr=initialize_root_path;
-eval(sprintf('run %s;',([rootPathStr,'GeoDataMATLAB\setup.m'])));
+eval(sprintf('run %s;',([rootPathStr,'energy-height-conversion\GeoDataMATLAB\setup.m'])));
 end
 
