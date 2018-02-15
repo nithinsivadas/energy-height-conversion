0040a322d0e84ec25b70157995f0e4486e5d1f77
nithinsivadas <nithin@bu.edu>
2018-02-15 12:43:08 -0500

    Added wildcard parameter

--- Tools/Get/get_files_in_folder.m
+++ Tools/Get/get_files_in_folder.m
@@ -1,8 +1,11 @@
-function [fileStr] = get_files_in_folder(dirName)
+function [fileStr] = get_files_in_folder(dirName,wildcard)
 %get_files_in_folder.m Produces a cell array of all file names in folder 
 %   Detailed explanation goes here
+if nargin<2
+    wildcard='*';
+end
 
-files = dir(dirName);
+files = dir([dirName,'/',wildcard]);
 fileIndex = find(~[files.isdir]);
 fileTempCells  = struct2cell(files);
 fileStr = fileTempCells(1,fileIndex);
