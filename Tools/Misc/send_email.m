4da367248aa724c9daa88c1e94bcfcd1f3769ff2
Nithin Sivadas <nithin@bu.edu>
2019-06-01 11:41:59 -0400

    run script and send email

---
+++ Tools/Misc/send_email.m
@@ -0,0 +1,56 @@
+function send_email(varargin)
+%send_email Sends an email from your gmail id set by preferences. 
+%-------
+% INPUT:
+%       to          - Recepient's email id
+%       subject     - Subject of the email
+%       message     - Message to be sent [has a default format]
+%       scriptName  - Name of the script executing this function, will be
+%                     included in the default message
+%       attachments - File paths to be attached, can be a cell array of
+%                     strings
+
+% Make sure your preferences are already set
+%setpref('Internet','SMTP_Server','smtp.gmail.com');
+%setpref('Internet','E_mail','nithin.iitm@gmail.com');
+%setpref('Internet','SMTP_Username','myusername');
+%setpref('Internet','SMTP_Password','mypassword');
+
+p = inputParser;
+
+addParameter(p,'to','nithin@bu.edu');
+addParameter(p,'subject','Code executed');
+addParameter(p,'attachments',nan);
+addParameter(p,'scriptName','A MATLAB script');
+nowTimeStr = datestr(clock);
+
+addParameter(p,'message',nan);
+
+parse(p,varargin{:});
+
+scriptName = p.Results.scriptName;
+
+if isnan(p.Results.message)
+    message = ['Hello'...
+        10 10 scriptName,' has been executed'...
+        10 'on ',get_computer_name...
+        10 'at ',nowTimeStr...
+        10 10 'Thank you'...
+        10 '- MATLAB ',version];
+end
+
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+subject = ['[MATLAB] ',p.Results.subject,' ',nowTimeStr];
+
+if ~isnan(p.Results.attachments)
+    sendmail(p.Results.to,subject,message,p.Results.attachments);
+else
+    sendmail(p.Results.to,subject,message);
+end
+
+
+end
---
+++ Tools/Routines/Tests/runAMISRCreateScript.m
@@ -0,0 +1,23 @@
+%% Linux create omni script
+diary record.txt
+startTimeStr = (datevec(datetime('now')));
+disp(['Start time: ',datestr(startTimeStr)]);
+try 
+    %% Your Code
+    dataStoreDir = "/media/nithin/PFISR_002_006/Nithin/";
+    outputFileStr = "amisrWebDatabase.h5";
+    timeMinStr = '1 Dec 2006';
+    timeMaxStr = '1 Jun 2019';
+    status = create_amisr_web_experiment_H5_database(timeMinStr,timeMaxStr,61,strcat(dataStoreDir,outputFileStr));
+    % Code ends
+catch ME
+    ME
+end
+endTimeStr = (datevec(datetime('now')));
+disp(['End time: ',datestr(endTimeStr)]);
+disp('-----------------------------------------');
+disp('              Complete');
+disp('-----------------------------------------');
+diary off
+send_email('attachments','record.txt','scriptName',mfilename);
+system('rm record.txt');
