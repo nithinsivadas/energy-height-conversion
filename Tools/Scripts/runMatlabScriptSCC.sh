0e5e09b03e186971cb93bfefab3aee68c47d71f9
Nithin Sivadas <nithin@scc-lite.bu.edu>
2019-06-04 15:57:28 -0400

    update scc script

--- Tools/Scripts/runMatlabScriptSCC.sh
+++ Tools/Scripts/runMatlabScriptSCC.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 functionHandle=$1
-echo "Running $1"
+echo "Running $1 on scc-lite"
 currentPath=$(pwd) 
 cd /usr3/graduate/nithin/MATLAB/
 nohup matlab -nodisplay -nosplash -r "run_script($1); quit">/dev/null 2>&1 &
+#nohup matlab -nodisplay -nosplash -r "run_script($1); quit">"/usr3/graduate/nithin/git-repos/nohup.txt"&
 cd $currentPath
-#nohup /usr/local/MATLAB/R2018b/bin/matlab -nodisplay -nosplash -r "run_script($1); quit">"/home/nithin/Documents/git-repos/nohup.txt" &
-echo "Run complete"
+echo "Now you can run something else"
