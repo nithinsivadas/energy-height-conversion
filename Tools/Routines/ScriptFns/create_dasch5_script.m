7a67cfd3d69b47f8c0698a8732608364d27d329d
Nithin Sivadas <nithin@bu.edu>
2020-12-01 01:52:31 -0500

    improved error verbosity

--- Tools/Routines/ScriptFns/create_dasch5_script.m
+++ Tools/Routines/ScriptFns/create_dasch5_script.m
@@ -13,5 +13,12 @@ function create_dasch5_script()
     [status, error] = create_dasc_H5_database(timeMinStr, timeMaxStr, [dataPath,outputFileStr]);
     disp([10 'Status: ']);
     cellstr(status)
+    
+    disp([10 'Errors: ']);
+    disp([error.directory.name,error.directory.message]);
+    disp([error.subdirectory.name,error.subdirectory.message]);
+    disp([10 ' ']);
+    disp(['The last program error: ',error.program.message]);
+    
     disp([10 'Data stored in ',dataPath,outputFileStr, 10]);
 end
