757172595602ca817e7468db86bac4749a327b1d
Nithin Sivadas <nithin@bu.edu>
2020-12-05 05:48:09 -0500

    scripts

--- Tools/Scripts/backupProjectnb.sh
+++ Tools/Scripts/backupProjectnb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rsyncs all files in $dataDir with auroraOptiplex except for those excluded in 'exclude-data.txt' 
-rsync -auv --info=progress2 --exclude-from='/usr3/graduate/nithin/git-repos/energy-height-conversion/Tools/Scripts/exclude-data.txt' $projectNbData/ nithin@128.197.173.50:/media/nithin/Elements/Nithin/Data/scc
+rsync -auv --info=progress2 --exclude-from='/usr3/graduate/nithin/git-repos/energy-height-conversion/Tools/Scripts/exclude-data.txt' /projectnb/semetergrp/nithin/Data/ nithin@128.197.173.50:/media/nithin/Elements/Nithin/Data/projectnb
