f0a77b339bed4777a77abe13813da35bd0ecb87a
Nithin Sivadas <nithin@bu.edu>
2022-04-29 10:57:18 -0400

    updating script for backing up from scc to aurora-optiplex

--- Tools/Scripts/backupProjectnb.sh
+++ Tools/Scripts/backupProjectnb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rsyncs all files in $dataDir with auroraOptiplex except for those excluded in 'exclude-data.txt' 
-rsync -auv --info=progress2 --exclude-from='/usr3/graduate/nithin/git-repos/energy-height-conversion/Tools/Scripts/exclude-data.txt' /projectnb/semetergrp/nithin/Data/ nithin@128.197.173.50:/media/nithin/Elements/Nithin/Data/projectnb
+rsync -auv --info=progress2 --exclude-from='/usr3/graduate/nithin/git-repos/energy-height-conversion/Tools/Scripts/exclude-data.txt' '/projectnb/semetergrp/nithin/Data/' nithin@128.197.173.50:/media/nithin/Elements/Nithin/Data/projectnb
